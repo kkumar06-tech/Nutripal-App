b040c26ee2d46dde4bd890bc57af122dc806ba13
unknown <ttrinh@hof-university.de>
2024-11-28 10:13:11 +0100

    changed

--- app/Models/WaterLog.php
+++ app/Models/WaterLog.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class WaterLog extends Model
 {
+    public function water()
+    {
+    return $this->belongsToMany(Water::class);
+    }
     public function userProfile()
     {
         return $this->belongsTo(UserProfile::class, 'id');
